d101408d0f5909e7ea8ae92206e1e8f7d40965a2
Alexander Emelyanov <contact.emelyanov@gmail.com>
2020-10-09 20:43:27 +0300

    убрал какой то мусор

--- Assets/Code/View/DragIndicatorUI.cs
+++ Assets/Code/View/DragIndicatorUI.cs
@@ -1,23 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Unity.Jobs.LowLevel.Unsafe;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class DragIndicatorUI : MonoBehaviour
 {
     [SerializeField]
-    public LineRenderer lr;
+    private LineRenderer lr;
 
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 startTouchPosition = Vector3.zero;
+
+    private Vector3 startWorldPosition;
+
+    private const float jopa = 146.2379631140851966704749469561f;
+
+    private void Start()
     {
-        //camera = Camera.main;
         lr = GetComponent<LineRenderer>();
     }
 
-    void RenderLine(Vector3 startPoint, Vector3 endPoint)
+    private void RenderLine(Vector3 startPoint, Vector3 endPoint)
     {
         lr.positionCount = 2;
         Vector3[] points = new Vector3[2];
@@ -32,21 +35,15 @@ public class DragIndicatorUI : MonoBehaviour
         lr.positionCount = 0;
     }
 
-    private Vector2 _StartMousePos = Vector3.zero;
-    private Vector3 startPosition;
-
-    private const float jopa = 414 / 2.83f;
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _StartMousePos = Input.mousePosition;
+            startTouchPosition = Input.mousePosition;
 
             float xPos = MathUtil.Map(Input.mousePosition.x, 0, Screen.width, -Screen.width / 2, Screen.width / 2);
             float yPos = MathUtil.Map(Input.mousePosition.y, 0, Screen.height, -Screen.height / 2, Screen.height / 2);
-            startPosition = new Vector3(xPos / jopa, yPos / jopa, 0);
+            startWorldPosition = new Vector3(xPos / jopa, yPos / jopa, 0);
         }
 
         if (Input.GetMouseButton(0))
@@ -57,7 +54,7 @@ public class DragIndicatorUI : MonoBehaviour
             float yPos = MathUtil.Map(Input.mousePosition.y, 0, Screen.height, -Screen.height / 2, Screen.height / 2);
             Vector3 thisPosition = new Vector3(xPos / jopa, yPos / jopa, 0);
 
-            RenderLine(startPosition, thisPosition);
+            RenderLine(startWorldPosition, thisPosition);
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -66,6 +63,3 @@ public class DragIndicatorUI : MonoBehaviour
         }
     }
 }
-
-
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
